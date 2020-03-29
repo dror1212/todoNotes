78b6c96dc7267fa89419b9492901778c772321a9
Dror Tal <dror.tal@ls-techs.com>
2020-03-29 17:35:30 +0300

    Updates

---
+++ precache-manifest.144623c0e7142a6ca2dd28306212e683.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b867a67383820505bda69ebb8bf52a9e",
+    "url": "/todoNotes/index.html"
+  },
+  {
+    "revision": "7c01a59ed1da8ed9a36d",
+    "url": "/todoNotes/static/css/2.534d1a7c.chunk.css"
+  },
+  {
+    "revision": "7083a46887d205cb865b",
+    "url": "/todoNotes/static/css/main.3519a276.chunk.css"
+  },
+  {
+    "revision": "7c01a59ed1da8ed9a36d",
+    "url": "/todoNotes/static/js/2.1c2bbcf7.chunk.js"
+  },
+  {
+    "revision": "14b6951eb3599e7c836b2179fd9b2b8b",
+    "url": "/todoNotes/static/js/2.1c2bbcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7083a46887d205cb865b",
+    "url": "/todoNotes/static/js/main.96e81843.chunk.js"
+  },
+  {
+    "revision": "6545648dcadd0a23b61b",
+    "url": "/todoNotes/static/js/runtime-main.9fd1f65c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/todoNotes/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/todoNotes/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/todoNotes/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/todoNotes/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/todoNotes/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/todoNotes/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/todoNotes/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/todoNotes/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/todoNotes/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/todoNotes/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/todoNotes/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/todoNotes/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/todoNotes/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/todoNotes/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/todoNotes/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/todoNotes/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
